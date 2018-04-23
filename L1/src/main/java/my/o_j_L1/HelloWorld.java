95ed0850ea23fe30ff8e74abff2ca7867fca558b
Grechka <grechka@MacBook-Pro-Grigorij.local>
2018-04-23 11:14:27 +0300

    first commit

---
+++ L1/src/main/java/my/o_j_L1/HelloWorld.java
@@ -0,0 +1,11 @@
+package my.o_j_L1;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println(NumberUtils.compare(0,1));
+    }
+}
